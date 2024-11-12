c3b4ae693554a2f314726250f74fea623321c418
Alexandre TL <alextorresleguet@icloud.com>
2024-11-12 10:29:15 +0100

    scripts and data

---
+++ init.sh
@@ -0,0 +1,6 @@
+pip install tqdm tikoken datasets
+pip install wandb
+wandb login 6677ca4ea7a459489b4dbbcd2503acd8e7de27d2
+pip install packaging
+pip install ninja
+pip install flash-attn --no-build-isolation
